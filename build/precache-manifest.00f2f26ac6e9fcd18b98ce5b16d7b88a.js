4f84e261d38c29c48f6f6d8256af961c95cee467
SachinMaiya <sachinm@quinnox.com>
2019-11-12 16:13:04 +0530

    second commit

---
+++ build/precache-manifest.00f2f26ac6e9fcd18b98ce5b16d7b88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cc181b812656de9178dd2af984f16f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "227dd432dae37558def5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6371b22c9754732c69f4",
+    "url": "/static/js/2.25f4a1ed.chunk.js"
+  },
+  {
+    "revision": "227dd432dae37558def5",
+    "url": "/static/js/main.210ef47e.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
